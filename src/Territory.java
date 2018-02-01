bb94297a12137c40ad9a9cac3a2b07583ce60fa4
Brian Wei <bwei@Brians-MBP.wv.cc.cmu.edu>
2018-01-31 22:44:00 -0500

    hi

---
+++ src/Territory.java
@@ -0,0 +1,9 @@
+
+public class Territory {
+	public String name;
+	public Territory[] neighborsF;
+	public Territory[] neighborsA;
+	public int occupied; //0: unoccupied, 1: army, -1: fleet
+	public boolean supplyCenter;
+	
+}
