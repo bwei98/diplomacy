dbd9ed7849d9e86c77c2b616d3941015b9813c39
bwei98 <bwei98@protonmail.com>
2018-02-21 22:20:36 -0500

    2018.02.21

--- src/Territory.java
+++ src/Territory.java
@@ -1,9 +1,17 @@
 
-public class Territory {
+public class Territory{
 	public String name;
 	public Territory[] neighborsF;
 	public Territory[] neighborsA;
 	public int occupied; //0: unoccupied, 1: army, -1: fleet
-	public boolean supplyCenter;
-	
+	public boolean supplyCenter; //true=yes
+
+	public Territory(String name, Territory[] nF, Territory[] nA, int occ, boolean sc) {
+        this.name = name;
+        this.neighborsF = nF;
+        this.neighborsA = nA;
+        this.occupied = occ;
+        this.supplyCenter = sc;
+    }
+
 }
---
+++ src/Unit.java
@@ -0,0 +1,6 @@
+public class Unit {
+    public int owner;
+    public boolean type; //true=fleet, false=army
+    public Territory location;
+    
+}
