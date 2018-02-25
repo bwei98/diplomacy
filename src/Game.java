e2500157a27541ef769a7255476f722ebd348c95
bwei98 <bwei98@protonmail.com>
2018-02-24 22:49:08 -0500

    start resolution and execution

--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,23 @@
+import javax.lang.model.type.ArrayType;
 import java.util.ArrayList;
 
 public class Game {
     public Country[] countries;
     public Territory[] territories;
-    public boolean doIneedafuckingretreat;
-    //Countries know what units they have, units know where they are so that should be enough^
+    public Country[] countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded;
+    //Countries know what units they have, units know where they are so that should be enough
 
 
     /**
      * Basic constructor for a Game state
      * @param countries Country[] for what the countries are
      * @param territories Territory[] of all the TERRITORIES
-     * @param doIneedafuckingretreat Whether you need a fucking retreat phase or not
+     * @param countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded Whether you need a fucking retreat phase or not
      */
-    public Game(Country[] countries, Territory[] territories, boolean doIneedafuckingretreat) {
+    public Game(Country[] countries, Territory[] territories, Country[] countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded) {
         this.countries = countries;
         this.territories = territories;
-        this.doIneedafuckingretreat = doIneedafuckingretreat;
+        this.countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded = countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded;
     }
 
     /**
@@ -27,7 +28,7 @@ public class Game {
     public Game(Country[] countries, Territory[] territories) {
         this.countries = countries;
         this.territories = territories;
-        this.doIneedafuckingretreat = false;
+        this.countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded = new Country[0];
     }
 
     /**
@@ -36,7 +37,7 @@ public class Game {
     public Game() {
         this.countries = null;
         this.territories = null;
-        this.doIneedafuckingretreat = false;
+        this.countriesthatneedtomakeafuckingretreatwhichwillbeemptyifnoneareneeded = new Country[0];
     }
 
     /**
@@ -45,24 +46,75 @@ public class Game {
      * @return Game representing the new gamestate
      */
     public Game movephase(String[][] orders) {
-        ArrayList<Move> Moves = new ArrayList<>();
+        ArrayList<Move> moves = new ArrayList<>();
+        ArrayList<Move> retreats = new ArrayList<>();
         for(int id=0; id<orders.length; id++) {
             for(String o : orders[id]) {
                 String[] order = o.split(" ");
-                //order[0]=A or F, order[1]=start loc, order[2]=type 1, order[3]=dest, order[4]="-", order[5]=dest2
                 for(String s: order)    s=s.trim();
                 Move m=new Move(this, id, order);
-                Moves.add(m);
+                moves.add(m);
             }
         }
-        /*
-        while(some moves are pending)
-            resolve them
-         */
+        while(countPending(moves)>0)
+            resolution(moves);
+
+        while(countExecutable(moves)>0)
+            retreats = executaion(moves, retreats);
+
+
         //TODO returning
         return new Game();
     }
 
+    /**
+     * Resolves at least one pending move
+     * @param moveset arraylist of moves
+     */
+    private void resolution(ArrayList<Move> moveset){
+        //@ENSURES countPending(moveset)>countPending(moveset')
+        //TODO
+    }
+
+    /**
+     * Resolves at least one executable move
+     * @param moveset arraylist of moves
+     * @param retreatsQueued arraylist of retreats needed
+     * @return updated of arraylist of retreats needed
+     */
+    private ArrayList<Move> executaion(ArrayList<Move> moveset, ArrayList<Move> retreatsQueued) {
+        //@ENSURES countExecutable(moveset)>countExecutable(moveset')
+        //TODO
+        
+        return retreatsQueued;
+
+    }
+
+    /**
+     * Number of PENDING orders in a moveset
+     * @param moveset an ArrayList of moves
+     * @return int for the number of pending orders in the list
+     */
+    private static int countPending(ArrayList<Move> moveset) {
+        int numPending=0;
+        for(Move a : moveset)
+            if(a.status==Status.PENDING)
+                numPending++;
+        return numPending;
+    }
+
+    /**
+     * Number of EXECUTable orders in a moveset
+     * @param moveset an ArrayList of moves
+     * @return int for the number of executable orders in the list
+     */
+    private static int countExecutable(ArrayList<Move> moveset) {
+        int numPending=0;
+        for(Move a : moveset)
+            if(a.status==Status.EXECUTABLE)
+                numPending++;
+        return numPending;
+    }
 
 
 
